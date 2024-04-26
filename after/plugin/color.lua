ca0f449b06020f157ab99e1fb5c79a4092133171
walrus947 <ethan.m.hartman@gmail.com>
2024-04-26 11:54:34 -0400

    feat: start of v2.0 -> complete rewrite

--- after/plugin/color.lua
+++
@@ -1,5 +0,0 @@
-vim.g.toykonight_transparent_sidebar = true
-vim.g.toykonight_transparent = true
-vim.opt.background = "dark"
-
-vim.cmd("colorscheme kanagawa")
--- after/plugin/lsp_lines.lua
+++
@@ -1,5 +0,0 @@
-vim.diagnostic.config({ 
-    virtual_lines = true,
-    virtual_text = false,
-})
-
